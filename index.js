cf33348f4ff8db45d31c1101807140cf4eaa6eb1
baraza7 <barazabrian87@gmail.com>
2023-06-16 12:24:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Peter Pan",
+    streetAddress: "Never Land"
+}
+
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+  return {...object, [key] : value}
+
+}console.log(updateEmployeeWithKeyAndValue(employee, "Santa", 4512))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value
+    return object
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "antman", 254)
+console.log(employee);
+
+function deleteFromEmployeeByKey(object, key){
+    const newObject = {
+        ...object
+    }
+    delete newObject[key]
+
+    return newObject
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object
+}
